7072d331fa997348a663d6fa1da90ef4fd0f0077
Lukas <lukasbhorak@gmail.com>
2018-02-01 16:43:04 -0500

    fixed up message, functionality

--- senateBot.py
+++ senateBot.py
@@ -1,18 +1,21 @@
-# senateBot
+# senateBot.py
 
 import praw
-#from PyDictionary import PyDictionary
-#import enchant
 import re
 
-# reddit API login, creds via praw.ini
+# Reddit API login, creds via praw.ini
 reddit = praw.Reddit('senateBot', user_agent='senate_bot by Lukas Horak v0.2')
 
-# active Subs
+# active Subs - switch out which one is commented-out for easy testing purposes
 subreddit = reddit.subreddit('testingground4bots')
+#subreddit = reddit.subreddit('prequelmemes')
 
-# phrase to activate the bot
+# Phrase which triggers senateBot
 keyword = 'senate'
+
+'''
+TODO - Check replies to comments? (for "not yet")
+'''
 
 # Find phrase and inform them of who the Senate is
 for comment in subreddit.stream.comments():
@@ -22,10 +25,14 @@ for comment in subreddit.stream.comments():
             hits = re.findall(r"[^.]*?senate[^.]*\.", comment.body, flags=re.I)
             for phrase in hits:
                 phr = re.compile(re.escape('senate'), re.IGNORECASE)
-                ph = phr.sub('***SENATE***', phrase)
-                reply += "> " +  ph + "\n\n[I ***AM*** THE SENATE](https://www.youtube.com/watch?v=_MHusGl9BeM)\n\n"
-            comment.reply(reply)
+                reply += "> " + phr.sub('***SENATE***', phrase) + "\n\n"
+            reply += "\n\n[I ***AM*** THE SENATE](https://www.youtube.com/watch?v=_MHusGl9BeM)\n\n"
+            reply += "---\n\nThis is a bot designed to make sure the treasonous Jedi realise Chancellor Palpatine IS the senate"
+            #comment.reply(reply)
+            print(reply)
 
             print("posted")
-        except:
-            print("fail")
+        except Exception as e:
+            print("senateBot fucked up...\n\n", e)
+
+# URL for "Not Yet" - (https://www.youtube.com/watch?v=03HOhd4GI1w#t=27s)
